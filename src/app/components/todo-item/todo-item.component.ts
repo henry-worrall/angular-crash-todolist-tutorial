70bac2935996a0f773ba7e1561e4062d1e5bf05b
Henry Worrall <61287647+henry-worrall@users.noreply.github.com>
2020-08-25 13:01:37 +0100

    Angular tutorial

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TodosComponent } from '../todos/todos.component';
+import { Todo } from '../../models/Todo';
+
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // Set Dynamic Classes
+  setClass() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('delete');
+  }
+}
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,5 @@
+export class Todo {
+    id:number; // can make optional using id?:number;
+    title:string;
+    completed: boolean;
+}
